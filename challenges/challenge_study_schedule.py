115756227508192239c506d65c2dd57eed7e348d
Eduardo Klein <eduardo.klein@live.com>
2024-03-09 16:25:02 -0300

    Requisito 1 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if not target_time:
+        return None
+
+    count = 0
+    for period in permanence_period:
+        if isinstance(period[0], int) and isinstance(period[1], int):
+            if target_time in range(period[0], period[1] + 1):
+                count += 1
+        else:
+            return None
+    return count
     raise NotImplementedError
